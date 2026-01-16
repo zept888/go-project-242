131d6e9fc3ab3525773c4ff6045c8bf85a020b99
zept <85477711+zept888@users.noreply.github.com>
2026-01-16 19:45:45 +0300

    Add GetSize() function + some minor tweaking

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -13,13 +13,44 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "hexlet-path-size",
 		Usage: "print size of a file or directory",
-		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("Hello from Hexlet!")
-			return nil
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			path := cmd.Args().First()
+			if path == "" {
+				return fmt.Errorf("path is required")
+			}
+			return GetSize(path)
 		},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func GetSize(path string) error {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return fmt.Errorf("failed to Lstat %s: %w", path, err)
+	}
+
+	if !info.IsDir() {
+		size := info.Size()
+		fmt.Printf("%d\t%s\n", size, path)
+	} else {
+		var totalSize int64
+		files, err := os.ReadDir(path)
+		if err != nil {
+			return fmt.Errorf("failed to read dir %s: %w\n", path, err)
+		}
+		for _, file := range files {
+			info, err := file.Info()
+			if err != nil {
+				return fmt.Errorf("failed to get file info for %s: %w", file.Name(), err)
+			}
+			totalSize += info.Size()
+
+		}
+		fmt.Printf("%d\t%s\n", totalSize, path)
+	}
+	return nil
 }
